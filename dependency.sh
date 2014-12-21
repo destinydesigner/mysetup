a125a32ba8600ef22c80152f79571b7328afa11a
Fang Guojian <dd.destinydesigner@gmail.com>
2014-12-21 20:23:09 +0800

    add exuberant ctags into dependencies

--- dependency.sh
+++ dependency.sh
@@ -1,3 +1,16 @@
 #!/bin/sh --
+# install pip
 sudo python get-pip.py
-sudo gem install rails
+
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install exuberant ctags for tagbar
+brew install ctags-exuberant
+
+# install rails
+rails -v
+if [ ! $? -eq 0 ]
+then
+    sudo gem install rails
+fi
